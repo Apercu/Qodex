7d44699d942c2fe66ef61024e598a5f18ee1a928
meriadec <meriadec.pillet@gmail.com>
2015-02-21 15:46:16 +0100

    feat(core): dev app id

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -1,6 +1,12 @@
 'use strict';
 
 module.exports = {
+
+  facebook: {
+    appId: '355141184678312',
+    secret: '091956ee2b03e965d3db424bdd6814e4'
+  },
+
   mongo: {
     uri: 'mongodb://localhost/qodex-dev'
   }
