d65d8b5d24b6bef8c66df4df16d3a160fa660f7a
meriadec <meriadec.pillet@gmail.com>
2015-02-21 21:17:13 +0100

    feat(core): full-pane directive

---
+++ client/directives/full-pane/full-pane.directive.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('qodex')
+  .directive('fullPane', function () {
+    return {
+      restrict: 'EA',
+      transclude: true,
+      templateUrl: 'directives/full-pane/full-pane.html',
+      link: function (scope, element) {
+      }
+    };
+  });
---
+++ client/directives/full-pane/full-pane.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('Directive: full-pane', function () {
+
+  beforeEach(module('qodex', 'templates'));
+
+  var element, scope;
+
+  beforeEach(inject(function($compile, $rootScope){
+    scope = $rootScope.$new();
+    element = angular.element('<full-pane></full-pane>');
+    element = $compile(element)(scope);
+    scope.$apply();
+  }));
+
+  it('should ...', function () {
+    expect(1).toBe(1);
+  });
+});
