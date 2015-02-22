d77ce0029164e294fec131f4e2d97044818322c3
meriadec <meriadec.pillet@gmail.com>
2015-02-22 09:05:30 +0100

    fix(core): core fix

--- client/directives/side-user/side-user.directive.js
+++ client/directives/side-user/side-user.directive.js
@@ -7,24 +7,24 @@ angular.module('qodex')
       replace: true,
       templateUrl: 'directives/side-user/side-user.html',
       link: function (scope, element) {
-        scope.vm = {
+        scope.model = {
           toggle: false
         };
         element.find('a').on('click', function () {
-          scope.vm.toggle = false;
+          scope.model.toggle = false;
           scope.$apply();
         });
         $document.on('click', function (e) {
           if (e.target === element || e.target === element.find('img')[0]) {
             return;
           }
-          scope.vm.toggle = false;
+          scope.model.toggle = false;
           scope.$apply();
         });
         document.onkeydown = function(evt) {
           evt = evt || window.event;
           if (evt.keyCode === 27) {
-            scope.vm.toggle = false;
+            scope.model.toggle = false;
             scope.$apply();
           }
         };
--- client/views/hello/hello.controller.js
+++ client/views/hello/hello.controller.js
@@ -2,8 +2,7 @@
 
 angular.module('qodex')
   .controller('HelloCtrl', function ($timeout, $location) {
-    // hack, see https://github.com/jaredhanson/passport-facebook#issues
-    window.location.hash = '';
+    $location.hash('');
     $timeout(function () {
       $location.path('/');
     }, 2500);
